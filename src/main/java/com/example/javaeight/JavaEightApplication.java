299b5d39ada410fe13904e6dbcc756f8cd621db3
Gaddam Ramaraju <gramaraju@hyd-lap-00758.americas.gap.com>
2023-11-08 16:07:04 +0530

    added the initial config changes

---
+++ src/main/java/com/example/javaeight/JavaEightApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaeight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaeight/JavaEightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaeight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
